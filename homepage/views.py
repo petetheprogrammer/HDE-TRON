e052f0e050f9913ba9a6e8b4313b389c6738f8af
Anon Tongngam <anon.tongngam@gmail.com>
2025-07-02 21:04:39 +0700

    some comments

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
+    #template = loader.get_template('home.html') #for template stuff
     return HttpResponse("Hello, world. You're at the homepage index.")
