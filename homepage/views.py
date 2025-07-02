2734ff25b8212199c9cec98c93fce5c7e56cfbf1
Anon Tongngam <anon.tongngam@gmail.com>
2025-07-02 21:53:31 +0700

    it works!!!

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
+
 
 
 def index(request):
     #template = loader.get_template('home.html') #for template stuff
-    return HttpResponse("Hello, world. You're at the homepage index.")
+    #return HttpResponse("Hello, world. You're at the homepage index.")
+    #return HttpResponse(template.render())
+    return render(request, "homepage/home.html")
