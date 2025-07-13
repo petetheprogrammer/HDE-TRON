12986f89e6d89dc27bc00abd1e82b2c6db99c50f
petetheprogrammer <135628321+petetheprogrammer@users.noreply.github.com>
2025-07-13 00:57:46 -0400

    connected db to django

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'aws-0-us-east-2.pooler.supabase.com',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PORT': '6543',
+        'PASSWORD': 'adminhde',
     }
 }
 
