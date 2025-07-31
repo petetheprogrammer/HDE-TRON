698d77a3bfbf84fd1a2b6b433f122cea2358ca29
petetheprogrammer <135628321+petetheprogrammer@users.noreply.github.com>
2025-07-31 08:28:55 -0400

    added statics for vercel to read css

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,8 +18,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -144,3 +144,4 @@ OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", "K85162163988957")
 LOGIN_URL          = 'login'         # named URL for the login view
 LOGIN_REDIRECT_URL = 'home'          # named URL for your homepage view
 LOGOUT_REDIRECT_URL = 'login'
+
